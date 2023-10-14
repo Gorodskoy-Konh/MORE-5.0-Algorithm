d86a9876afd7330902bbefe8aa6f96f6e2321747
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-14 21:46:13 +0300

    Add record estimate wait

--- bank_algorithms/bank_priority_sort_algorithm.py
+++ bank_algorithms/bank_priority_sort_algorithm.py
@@ -1,3 +1,4 @@
+from datetime import time, timedelta
 from .bank_unit import BankUnit
 from .bank_client import BankClient
 
@@ -26,3 +27,8 @@ def bank_priority_sort_algorithm(banks: list[BankUnit], client: BankClient, aver
     
     estimate_times.sort(key=lambda x: x[1])
     return estimate_times
+
+def recorded_estimate_wait(record_time: time, averate_wait_time: float) -> time:
+    record_timedelta = timedelta(hours=record_time.hour, minutes=record_time.minute, seconds=record_time.second)
+    estimated_wait = averate_wait_time + record_timedelta.seconds
+    return estimated_wait
