b986718efac5103471f3f82e639f15cbd5b131fe
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-15 02:16:47 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from bank_algorithms.bank_priority_sort_algorithm import __version__
 
 setup(
     name='bank_algorithms',
-    version=__version__,
+    version="0.1",
 
-    url='',
+    url='https://github.com/Gorodskoy-Konh/MORE-5.0-Algorithm',
     author='Nagim Isyanbaev',
     author_email='n.isyanbaev@innopol.university',
 
