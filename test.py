833fe61923241c93324861d04b24c959d2acba36
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-14 20:46:35 +0300

    add test

--- test.py
+++ test.py
@@ -30,19 +30,33 @@ clients2 = [
     BankClient("B"),
 ]
 
+clients3 = [
+    BankClient("C"),
+    BankClient("C"),
+    BankClient("C"), 
+    BankClient("B"),
+]
+
 queue1 = BankQueue(clients1)
 queue2 = BankQueue(clients2)
+queue3 = BankQueue(clients3)
 
 terminals1 = [
     BankTerminal(0, {"A": 2, "B": 1}),
     BankTerminal(1, {"B": 2, "A": 1}),
-    BankTerminal(1, {"C": 0.5}),
+    BankTerminal(2, {"C": 0.5}),
 ]
 terminals2 = [
     BankTerminal(0, {"A": 2}),
     BankTerminal(1, {"B": 3}),
-    BankTerminal(1, {"C": 0.5}),
+    BankTerminal(2, {"C": 0.5}),
+]
+
+terminals3 = [
+    BankTerminal(0, {"C": 1, "A": 2}),
+    BankTerminal(1, {"B": 2, "A": 1})
 ]
 bank1 = BankUnit(0, queue1, terminals1, 1)
 bank2 = BankUnit(1, queue2, terminals2, 2)
-print(bank_priority_sort_algorithm([bank1, bank2], moi_mujik))
+bank3 = BankUnit(2, queue3, terminals3, 1)
+print(bank_priority_sort_algorithm([bank1, bank2, bank3], moi_mujik, 0))
